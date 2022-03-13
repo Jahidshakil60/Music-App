9de9acfac1403663a764ae6c3e351a48501dbae4
Jahid Hasan <theshakil53@gmail.com>
2022-03-13 14:11:28 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./style/app.scss"
+import Player from "./components/Player";
+import Song from "./components/Song";
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
+
+
+
